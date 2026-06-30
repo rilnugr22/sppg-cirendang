c9eeca261aa0c003d2c5d93268a103fe2c895578
rilnugr22 <cowokpendia08@gmail.com>
2026-06-30 10:48:24 +0700

    change seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Admin SPPG',
+            'email' => 'admin@sppg.com',
+            'password' => Hash::make('password123'),
+     
         ]);
     }
 }
